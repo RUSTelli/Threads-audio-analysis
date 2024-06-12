a94393b25dc49a36e90473e1e02d9e17f457c545
RUSTelli <francescorasta02@gmail.com>
2024-06-12 04:27:41 +0200

    refactoring

--- train_nlp_model.py
+++ train_nlp_model.py
@@ -2,8 +2,12 @@ from nlp_model import TextClassifier
 from data_managing import data_pipeline
 
 
-dataset = data_pipeline()
-bertone = TextClassifier(num_labels=3, output_dir="sentiment_output")
+sentiment_dataset = data_pipeline(classification_type="ternary")
+hate_dataset      = data_pipeline(classification_type="multi")
+
+sentiment_classifier = TextClassifier(num_labels=3, output_dir="sentiment_output")
+hate_classifier      = TextClassifier(num_labels=7, output_dir="hate_output")
 
 
-bertone.train(dataset)
+sentiment_classifier.train(sentiment_dataset)
+hate_classifier.train(hate_dataset)

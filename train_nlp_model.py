fa1f51a55decc900641eac57c008205b57e4591f
DomenicoAnzalone <anzalone.domenico07@gmail.com>
2024-06-13 17:22:06 +0200

    Refactoring training file

--- train_nlp_model.py
+++ train_nlp_model.py
@@ -5,7 +5,7 @@ from data_managing import data_pipeline
 sentiment_dataset   = data_pipeline()
 #hate_dataset      = data_pipeline(classification_type="multi")
 
-sentiment_classifier = SentimentClassifier(num_labels=2, output_dir="sentiment_output", epochs=4)
+sentiment_classifier = SentimentClassifier()
 # #hate_classifier      = TextClassifier(num_labels=7, output_dir="hate_output")
 
 sentiment_classifier.train(sentiment_dataset, epochs=4, batch_size=64)
